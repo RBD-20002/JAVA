50d3e921cdf5bf48fb5f47cc036295ecd95e95ae
Ricardo Bautista Diaz <ricardo03-03-02@hotmail.com>
2025-03-18 18:57:27 +0100

    Crear Vehículo.java

---
+++ 2_Vehiculo/src/Vehículo.java
@@ -0,0 +1,12 @@
+public class Vehiculo{
+
+     private String marca;
+     private String modelo;
+     
+     public getMarca() {
+         return Marca;
+     }
+     public void setMarca(String Marca){
+         this.Marca = marca;
+     }
+}
