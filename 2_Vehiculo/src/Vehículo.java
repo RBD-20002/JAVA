62713cee5609562ae68511246581c7b658954dd1
Ricardo Bautista Diaz <ricardo03-03-02@hotmail.com>
2025-03-18 18:59:55 +0100

    Actualizar Vehículo.java

--- 2_Vehiculo/src/Vehículo.java
+++ 2_Vehiculo/src/Vehículo.java
@@ -3,10 +3,18 @@ public class Vehiculo{
      private String marca;
      private String modelo;
      
+
+     public Vehículo(String marca, String modelo){
+         this.marca = marca;
+         this.modelo = modelo;
+     }
+
+
      public getMarca() {
          return Marca;
      }
      public void setMarca(String Marca){
          this.Marca = marca;
      }
+
 }
