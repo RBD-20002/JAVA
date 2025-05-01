6f10c4e86edb01a6e8b9c61b04471ed39f2d993e
RICARDO <RICARDO03-03-02@HOTMAIL.com>
2025-05-02 00:17:00 +0200

    Proceso 7%

---
+++ 3.5_Biblioteca/src/InfoMenu.java
@@ -0,0 +1,13 @@
+public class InfoMenu {
+
+    public static void menu() {
+        System.out.println("""
+                |--GESTION BIBLIOTECA--|
+                |1. Añadir libro       |
+                |2. Listar Libros      |
+                |3. Buscar Libro       |
+                |4. Eliminar Libro     |
+                |5. Salir              |
+                """);
+    }
+}
--- 3.5_Biblioteca/src/Main.java
+++ 3.5_Biblioteca/src/Main.java
@@ -1,22 +1,9 @@
-import java.io.IOException;
-import java.lang.invoke.SwitchPoint;
-import java.nio.file.Files;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        try{
-            if(!Files.notExists(MetodosFichero)){
-                System.out.println("El fichero ya existe");
-                return;
-            }else{
-                System.out.println("El fichero se creo con exito");
-            }
-        }catch (IOException e){
-            System.out.println("ERROR: fallo al crear el archivo");
-        }
+        Biblioteca biblioteca = new Biblioteca();
 
         int opcion = -1;
         do{
@@ -26,16 +13,16 @@ public class Main {
                 opcion = Integer.parseInt(sc.nextLine());
                 switch (opcion){
                     case 1:
-                        MetodosFichero.agregarLibro();
+                        Biblioteca.agregarLibro();
                         break;
                     case 2:
-                        MetodosFichero.mostrarLibros();
+                        Biblioteca.mostrarLibros();
                         break;
                     case 3:
-                        MetodosFichero.buscarLibro();
+                        Biblioteca.buscarLibro();
                         break;
                     case 4:
-                        MetodosFichero.eliminarLibro();
+                        Biblioteca.eliminarLibro();
                         break;
                     case 5:
                         System.out.println("Hasta Luego.....");
@@ -46,6 +33,6 @@ public class Main {
             }catch (NumberFormatException e){
                 System.out.println("ERROR: dato introducido invalido");
             }
-        }while (opcion != 6);
+        }while (opcion != 5);
     }
 }
